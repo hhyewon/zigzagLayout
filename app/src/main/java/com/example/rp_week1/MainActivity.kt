2dea92561c382f2f0a2e8a6760e75e0de0ad4bab
hhyewon <diddidgpdnjs@naver.com>
2021-07-18 01:31:13 +0900

    중간 CardView 완료

--- app/src/main/java/com/example/rp_week1/MainActivity.kt
+++ app/src/main/java/com/example/rp_week1/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.rp_week1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
